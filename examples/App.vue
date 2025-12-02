49d600c5f1185b0325c7b6be97186b962112c69a
zero <zero.kwok@foxmail.com>
2025-12-02 18:33:33 +0800

    feat: 添加可调整宽度的侧边栏并优化图片列表展示

--- examples/App.vue
+++ examples/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="container">
+    <div class="sidebar" :class="{ 'collapsed': sidebarCollapsed }"
+      :style="{ width: sidebarCollapsed ? '0px' : sidebarWidth + 'px' }" ref="sidebarRef">
+      <div class="sidebar-handle" @mousedown="sidebarStartResize" :class="{ 'collapsed': sidebarCollapsed }"
+        @click="sidebarToggle">
+      </div>
+
+      <div class="sidebar-content">
+        <div class="image-list">
+          <div v-for="name in examples" :key="name" class="item" :class="{ active: name === currentName }"
+            @click="currentName = name">
+            <img :src="getItemImage(name, '2')" :alt="name" />
+            <span class="name">{{ name }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="image-viewer">
       <ImageSliderCompare ref="imageView" :left="currentLeft" :right="currentRight" v-model:zoom="zoom"
-        :zoomRange="zoomRange"/>
+        :zoomRange="zoomRange" />
 
       <div class="controls">
-        <el-slider class="zoom" v-model="zoom" :min="zoomRange.min" :max="zoomRange.max" :step="zoomRange.step" :vertical="true" height="80px"
-          :format-tooltip="val => `Zoom: ${val.toFixed(2)}%`" />
+        <el-slider class="zoom" v-model="zoom" :min="zoomRange.min" :max="zoomRange.max" :step="zoomRange.step"
+          :vertical="true" height="80px" :format-tooltip="val => `Zoom: ${val.toFixed(2)}%`" />
 
         <el-dropdown class="fit-mode" @command="mode => imageView.updateFitMode(mode)">
           <el-button plain>
@@ -26,20 +43,49 @@
       </div>
 
     </div>
-
-    <div class="image-list">
-      <div v-for="name in examples" :key="name" class="item" :class="{ active: name === currentName }"
-        @click="currentName = name">
-        <img :src="getItemImage(name, '2')" :alt="name" />
-        <span class="name">{{ name }}</span>
-      </div>
-    </div>
   </div>
 </template>
 
 <script setup>
 import { ref, computed } from "vue";
 import ImageSliderCompare from "vue3-image-compare-slider";
+
+// 侧边栏
+const sidebarWidth = ref(250)
+const sidebarMinWidth = 150
+const sidebarMaxWidth = 400
+const sidebarCollapsed = ref(false)
+
+const sidebarStartResize = (e) => {
+  e.preventDefault()
+  const startX = e.clientX
+  const startWidth = sidebarWidth.value
+
+  const handleMouseMove = (e) => {
+    const dx = e.clientX - startX
+    let newWidth = startWidth + dx
+
+    // 限制宽度范围
+    if (newWidth < sidebarMinWidth)
+      newWidth = sidebarMinWidth
+    else if (newWidth > sidebarMaxWidth)
+      newWidth = sidebarMaxWidth
+    sidebarWidth.value = newWidth;
+    sidebarCollapsed.value = newWidth <= sidebarMinWidth;
+  }
+
+  const handleMouseUp = () => {
+    document.removeEventListener('mousemove', handleMouseMove)
+    document.removeEventListener('mouseup', handleMouseUp)
+  }
+
+  document.addEventListener('mousemove', handleMouseMove)
+  document.addEventListener('mouseup', handleMouseUp)
+}
+
+const sidebarToggle = () => {
+  sidebarCollapsed.value = !sidebarCollapsed.value
+}
 
 const examples = [
   'portrait-265',
@@ -65,7 +111,9 @@ const currentRight = computed(() => getItemImage(currentName.value, '2'));
 
 const imageView = ref(null);
 const zoom = ref(100);
-const zoomRange = {min: 10, max: 400, step: 10};
+const zoomRange = { min: 10, max: 400, step: 10 };
+
+
 </script>
 
 <style lang="scss" scoped>
@@ -75,9 +123,77 @@ const zoomRange = {min: 10, max: 400, step: 10};
   background-color: #fff;
 
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   padding: 1rem;
   gap: 1.5rem;
+
+  .sidebar {
+    position: relative;
+    &.collapsed .sidebar-content {
+      display: none;
+    }
+
+    .sidebar-handle {
+      position: absolute;
+      top: 0px;
+      right: -4px;
+      width: 4px;
+      height: 100%;
+      background-color: #ddd8;
+      transition: background-color 0.2s;
+      cursor: ew-resize;
+
+      &.collapsed {
+        background-color: transparent;
+      }
+
+      &:hover { 
+        background-color: #ccca;
+      }
+    }
+
+    .image-list {
+      border: 1px solid #ccc;
+      padding: 3px 3px;
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(80px, 1fr));
+      gap: 0.6rem;
+
+      .item {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        cursor: pointer;
+        transition: all 0.2s ease;
+        padding: 0.5rem;
+
+        &:hover {
+          transform: translateY(-2px);
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+        }
+
+        &.active {
+          background-color: #e0f7fa;
+          box-shadow: 0 0 0 2px #000;
+        }
+
+        img {
+          width: 100%;
+          aspect-ratio: 1;
+          object-fit: cover;
+          border-radius: 4px;
+        }
+
+        .name {
+          margin-top: 0.5rem;
+          font-size: 0.8rem;
+          color: #424242;
+          text-align: center;
+          word-break: break-word;
+        }
+      }
+    }
+  }
 
   .image-viewer {
     flex-grow: 1;
@@ -147,48 +263,5 @@ const zoomRange = {min: 10, max: 400, step: 10};
       }
     }
   }
-
-  .image-list {
-    border: 1px solid #ccc;
-    padding: 3px 3px;
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(80px, 1fr));
-    gap: 0.6rem;
-
-    .item {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      cursor: pointer;
-      transition: all 0.2s ease;
-      padding: 0.5rem;
-
-      &:hover {
-        transform: translateY(-2px);
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
-      }
-
-      &.active {
-        background-color: #e0f7fa;
-        box-shadow: 0 0 0 2px #000;
-      }
-
-      img {
-        width: 100%;
-        aspect-ratio: 1;
-        object-fit: cover;
-        border-radius: 4px;
-      }
-
-      .name {
-        margin-top: 0.5rem;
-        font-size: 0.8rem;
-        color: #424242;
-        text-align: center;
-        word-break: break-word;
-      }
-    }
-  }
-
 }
 </style>
